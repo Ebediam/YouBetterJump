5340def401634c867f92779f9a64b64d50abeef2
Ebediam <felipebiosca@gmail.com>
2019-11-08 20:26:05 +0100

    Initial commit

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    public Transform spawnPoint;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInChildren<PlayerController>())
+        {
+            other.transform.root.position = spawnPoint.position;
+            other.transform.root.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    public float maxSpeed;
+    public float jumpForce;
+    public List<Transform> groundChecks;
+    public bool isGrounded = true;
+    public Ray groundRay;
+    public float rayLength;
+    public bool disableGroundCheck = false;
+
+    public float noGroundCheckTimer;
+
+    float groundTimer;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!disableGroundCheck)
+        {
+            isGrounded = false; 
+            foreach (Transform groundCheck in groundChecks)
+            {
+                groundRay = new Ray(groundCheck.position, groundCheck.forward);
+
+                if (Physics.Raycast(groundRay, rayLength))
+                {
+                    Debug.DrawLine(groundCheck.position, groundCheck.position + groundCheck.forward * rayLength, Color.green);
+                    isGrounded = true;
+                    break;
+                }
+                else
+                {
+                    Debug.DrawLine(groundCheck.position, groundCheck.position + groundCheck.forward * rayLength, Color.red);
+                }
+            }
+        }
+        
+
+
+        rb.AddForce(transform.forward * speed, ForceMode.Force);
+
+        if(rb.velocity.z > maxSpeed)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, maxSpeed);
+        }
+
+        if (Input.GetAxis("Jump")> 0)
+        {
+            if (isGrounded)
+            {   
+
+                Debug.Log("JUMP!");
+                rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+                isGrounded = false;
+                disableGroundCheck = true;
+            }
+            else
+            {
+                Debug.Log("Key pressed but not on the ground");
+            }
+        }
+
+    }
+
+    public void Update()
+    {
+        if (disableGroundCheck)
+        {
+            groundTimer += Time.deltaTime;
+            if(groundTimer >= noGroundCheckTimer)
+            {
+                disableGroundCheck = false;
+                groundTimer = 0f;
+            }
+        }
+
+
+    }
+    public void ResetJump()
+    {
+
+
+    }
+}
